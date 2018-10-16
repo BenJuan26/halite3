646478398d665e902ad33523cd1cee3af3750d9b
Benjamin Schubert <bschubert@ca.ibm.com>
2018-10-16 19:31:51 -0400

    Use custom hlt package

--- MyBot.go
+++ MyBot.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"hlt"
-	"hlt/gameconfig"
-	"hlt/log"
 	"math/rand"
 	"os"
 	"os/signal"
 	"strconv"
 	"syscall"
 	"time"
+
+	"github.com/BenJuan26/hlt"
+	"github.com/BenJuan26/hlt/gameconfig"
+	"github.com/BenJuan26/hlt/log"
 )
 
 func gracefulExit(logger *log.FileLogger) {
@@ -26,6 +27,11 @@ func gracefulExit(logger *log.FileLogger) {
 		os.Exit(0)
 	}()
 }
+
+const (
+	exploring = 1
+	returning = 2
+)
 
 func main() {
 	args := os.Args
@@ -46,21 +52,34 @@ func main() {
 	logger.Printf("Successfully created bot! My Player ID is %d. Bot rng seed is %d.", game.Me.ID, seed)
 	gracefulExit(fileLogger)
 	var maxHalite, _ = config.GetInt(gameconfig.MaxHalite)
+	var shipCost, _ = config.GetInt(gameconfig.ShipCost)
 	game.Ready("MyBot")
+	shipRoles := make(map[int]int)
 	for {
 		game.UpdateFrame()
 		var me = game.Me
 		var gameMap = game.Map
 		var ships = me.Ships
 		var commands = []hlt.Command{}
+
 		for i := range ships {
 			var ship = ships[i]
+			shipID := ship.GetID()
 
-			var currentCell = gameMap.AtEntity(ship.E)
-			if ship.IsFull() {
-				dir := gameMap.NaiveNavigate(ship, me.Shipyard.E.Pos)
-				commands = append(commands, ship.Move(dir))
-			} else if currentCell.Halite < (maxHalite / 10) {
+			if _, ok := shipRoles[shipID]; !ok {
+				shipRoles[shipID] = exploring
+			}
+
+			if shipRoles[shipID] == returning {
+				if ship.E.Pos.Equals(me.Shipyard.E.Pos) {
+					shipRoles[shipID] = exploring
+				} else {
+					dir := gameMap.NaiveNavigate(ship, me.Shipyard.E.Pos)
+					commands = append(commands, ship.Move(dir))
+				}
+			} else if ship.Halite > (maxHalite / 2) {
+				shipRoles[shipID] = returning
+			} else if gameMap.AtEntity(ship.E).Halite < (maxHalite / 10) {
 				potentialDirection := hlt.AllDirections[rand.Intn(4)]
 				potentialPos, _ := ship.E.Pos.DirectionalOffset(potentialDirection)
 				if gameMap.AtPosition(potentialPos).IsEmpty() {
@@ -70,7 +89,7 @@ func main() {
 				commands = append(commands, ship.Move(hlt.Still()))
 			}
 		}
-		var shipCost, _ = config.GetInt(gameconfig.ShipCost)
+
 		if game.TurnNumber <= 200 && me.Halite >= shipCost && !gameMap.AtEntity(me.Shipyard.E).IsOccupied() {
 			commands = append(commands, hlt.SpawnShip{})
 		}
--- create_zip.sh
+++ create_zip.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-set -e
 rm MyBot.zip
 zip MyBot MyBot.go
 dir=$PWD
 pushd $GOPATH > /dev/null
-zip -r ${dir}/MyBot src/hlt
+zip -r ${dir}/MyBot src/github.com/BenJuan26/hlt
 popd > /dev/null
